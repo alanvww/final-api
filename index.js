7662133f0b3461e8db4813730a44f36806dce18b
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-12-13 03:20:38 -0500

    Backend for all done

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require('express');
+// Initiate express
 const app = express();
-const port = process.env.PORT || 3000;
+// Setting port - dynamically with Heroku
+const port = process.env.PORT || 4000;
+// Import Firebase
+const firebase = require('firebase/app');
+// Get configuration object so we can communicate with firebase
+const firebaseConfig = {
+	apiKey: 'AIzaSyDXxPtJPCHNyb-s2ZACOhh1-qNSVfTVO7Y',
+	authDomain: 'final-project-4d11a.firebaseapp.com',
+	projectId: 'final-project-4d11a',
+	storageBucket: 'final-project-4d11a.appspot.com',
+	messagingSenderId: '82999155333',
+	appId: '1:82999155333:web:5bebe67b770e9adde267ec',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+// CORS Fix
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// Request methods you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	);
+	// Request headers you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	);
+	// Pass to next layer of middleware
+	next();
 });
 
+// routes for directing user to correct place
+const indexRoute = require('./routes/index');
+const articleRoute = require('./routes/article');
+const createArticleRoute = require('./routes/createArticle');
+// tell express to use routes
+app.use('/', indexRoute);
+app.use('/article', articleRoute);
+app.use('/create', createArticleRoute);
+// Listen for port
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const { response } = require('express');
+var express = require('express');
+// Middleware to allow for routing on the node server
+var router = express.Router();
+// require firestore
+const firestore = require('firebase/firestore');
+// Initialized Firestore database
+const db = firestore.getFirestore();
+// Reference to the blogposts
+router.get('/', (req, res) => {
+	const lists = firestore.getDocs(firestore.collection(db, 'lists'));
+	// Create empty array
+	const listsArray = [];
+
+	lists
+		.then((response) => {
+			response.forEach((doc) => {
+				// Push document into array every time the query loops over data and push into array
+				listsArray.push(doc.data());
+			});
+			return res.send(listsArray);
+		})
+		.catch(function (error) {
+			console.log('Error', error);
+			return res.send(error);
+		});
+});
+
+module.exports = router;
